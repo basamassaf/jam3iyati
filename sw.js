06d8dbd542cace8387bdaac21d21a321ba1ea4a7
basamassaf <1980basam@gmail.com>
2026-09-13 00:38:02 +0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = 'wk-cache-v1';
+const urlsToCache = [
+  './maintenance.html',
+  './eagle.png',
+  './manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache).catch(() => {}))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
+  );
+});
